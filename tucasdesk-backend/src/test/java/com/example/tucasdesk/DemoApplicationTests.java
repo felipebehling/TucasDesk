912644305da05ee8563b3fd1ef115fa319897c11
Felipe Behling <89941251+felipebehling@users.noreply.github.com>
2025-08-21 19:29:48 -0300

    adadad

--- tucasdesk-backend/src/test/java/com/example/tucasdesk/DemoApplicationTests.java
+++ tucasdesk-backend/src/test/java/com/example/tucasdesk/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.tucasdesk;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
