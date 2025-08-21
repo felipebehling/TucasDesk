cf09bdb43d6eed62754c0c191f999f0d6979f289
Felipe Behling <89941251+felipebehling@users.noreply.github.com>
2025-08-21 20:28:57 -0300

    AAA

--- tucasdesk-backend/src/test/java/com/example/tucasdesk/DemoApplicationTests.java
+++ tucasdesk-backend/src/test/java/com/example/tucasdesk/DemoApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.tucasdesk;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 
 @SpringBootTest
+@ActiveProfiles("test") // usa H2 durante os testes
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
